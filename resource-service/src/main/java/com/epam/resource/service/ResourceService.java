2dbcf2c85eaadb10d3523623d5ea36f77dec7135
Ievgen Gusar <ievgen_gusar@epam.com>
2024-11-04 19:10:44 +0200

    Implement extractMetadata method. Add support of 10Mb size multipart files

--- resource-service/src/main/java/com/epam/resource/service/ResourceService.java
+++ resource-service/src/main/java/com/epam/resource/service/ResourceService.java
@@ -4,10 +4,22 @@ import com.epam.resource.client.SongServiceClient;
 import com.epam.resource.domain.Resource;
 import com.epam.resource.exceptions.ResourceNotFoundException;
 import com.epam.resource.repository.ResourceRepository;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.mp3.Mp3Parser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -19,6 +31,8 @@ public class ResourceService {
 
     @Autowired
     private SongServiceClient songServiceClient;
+
+    Logger logger = LoggerFactory.getLogger(ResourceService.class);
 
     public Resource saveResource(MultipartFile file) throws Exception {
         Resource resource = new Resource();
@@ -44,8 +58,24 @@ public class ResourceService {
         resourceRepository.deleteAllById(ids);
     }
 
-    private Map<String, String> extractMetadata(byte[] data) {
-        // Use Apache Tika to extract metadata
-        return Map.of("Artist", "Queen", "Album", "Example");
+    public Map<String, String> extractMetadata(byte[] data) {
+        Map<String, String> metadataMap = new HashMap<>();
+        Mp3Parser parser = new Mp3Parser();
+
+        try (InputStream inputStream = new ByteArrayInputStream(data)) {
+            ContentHandler handler = new BodyContentHandler();
+            Metadata metadata = new Metadata();
+            parser.parse(inputStream, handler, metadata, new ParseContext());
+
+            // Retrieve metadata keys and store in map
+            String[] metadataNames = metadata.names();
+            for (String name : metadataNames) {
+                metadataMap.put(name, metadata.get(name));
+            }
+        } catch (IOException | SAXException | org.apache.tika.exception.TikaException e) {
+            logger.error("An error occurred during MP3 metadata extraction: {}", e.getMessage(), e);
+        }
+
+        return metadataMap;
     }
 }
