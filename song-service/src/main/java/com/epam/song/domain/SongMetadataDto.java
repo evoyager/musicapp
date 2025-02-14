b7f377171f3b17da06bacf6f4371085400941566
Ievgen Gusar <evgeniygusar@gmail.com>
2025-02-14 17:09:01 +0200

    Add duration validation

--- song-service/src/main/java/com/epam/song/domain/SongMetadataDto.java
+++ song-service/src/main/java/com/epam/song/domain/SongMetadataDto.java
@@ -23,6 +23,7 @@ public class SongMetadataDto {
 
     private String album;
 
+    @Pattern(regexp = "^\\d{2}:[0-5][0-9]$", message = "must be in the mm:ss format, where mm is positive and ss is between 00 and 59")
     private String duration;
 
     @Pattern(regexp = "^(19|20)\\d{2}$", message = "must be in YYYY format and between 1900 and 2099.")
